a66bf38e49599f2590da79f92667e70c9b2dcc4c
AlbinKryeziu <albinkryeziu21@gmail.com>
2020-11-02 21:10:54 +0100

    fixes design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,15 +8,9 @@
   <title>O-spitali</title>
   <meta content="" name="descriptison">
   <meta content="" name="keywords">
-
-  <!-- Favicons -->
   <link href="{{asset('homepages/assets/img/logo1.jpg')}}" rel="icon">
   <link href="{{asset('homepages/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
   <link href="{{asset('homepages/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
   <link href="{{asset('homepages/assets/vendor/icofont/icofont.min.css')}}" rel="stylesheet">
   <link href="{{asset('homepages/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
@@ -24,21 +18,11 @@
   <link href="{{asset('homepages/assets/vendor/owl.carousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
   <link href="{{asset('homepages/assets/vendor/venobox/venobox.css')}}" rel="stylesheet">
   <link href="{{asset('homepages/assets/vendor/aos/aos.css')}}" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
   <link href="{{asset('homepages/assets/css/style.css')}}" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Medicio - v2.0.0
-  * Template URL: https://bootstrapmade.com/medicio-free-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
 
-  <!-- ======= Top Bar ======= -->
   <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
       <div class="d-flex align-items-center">
@@ -52,8 +36,6 @@
   </div>
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
-
-      {{-- <a href="index.html" class="logo mr-auto"><img src="{{asset('homepages/assets/img/logo3.1.jpg')}}" alt=""></a> --}}
       <a style="">O-Spitali</a>
 
       <nav class="nav-menu d-none d-lg-block">
@@ -537,27 +519,7 @@
 
       </div>
     </section>
-    <section id="gallery" class="gallery">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Gallery</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-
-        <div class="owl-carousel gallery-carousel" data-aos="fade-up" data-aos-delay="100">
-          <a href="{{('homepages/assets/img/gallery/gallery-1.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-1.jpg')}}" alt=""></a>
-          <a href="{{('homepages/assets/img/gallery/gallery-2.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-2.jpg')}}" alt=""></a>
-          <a href="{{('homepages/assets/img/gallery/gallery-3.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-3.jpg')}}" alt=""></a>
-          <a href="{{('homepages/assets/img/gallery/gallery-4.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-4.jpg')}}" alt=""></a>
-          <a href="{{('homepages/assets/img/gallery/gallery-5.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-5.jpg')}}" alt=""></a>
-          <a href="{{('homepages/assets/img/gallery/gallery-6.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-6.jpg')}}" alt=""></a>
-          <a href="{{('homepages/assets/img/gallery/gallery-7.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-7.jpg')}}" alt=""></a>
-          <a href="{{('homepages/assets/img/gallery/gallery-8.jpg')}}" class="venobox" data-gall="gallery-carousel"><img src="{{('homepages/assets/img/gallery/gallery-8.jpg')}}" alt=""></a>
-        </div>
-
-      </div>
-    </section>
+    
     <section id="pricing" class="pricing">
       <div class="container" data-aos="fade-up">
 
@@ -641,74 +603,7 @@
 
       </div>
     </section>
-    <section id="faq" class="faq section-bg">
-      <div class="container" data-aos="fade-up">
-
-        <div class="section-title">
-          <h2>Frequently Asked Questioins</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-        </div>
-
-        <ul class="faq-list" data-aos="fade-up" data-aos-delay="100">
-
-          <li>
-            <a data-toggle="collapse" class="" href="#faq1">Non consectetur a erat nam at lectus urna duis? <i class="icofont-simple-up"></i></a>
-            <div id="faq1" class="collapse show" data-parent=".faq-list">
-              <p>
-                Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-toggle="collapse" href="#faq2" class="collapsed">Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque? <i class="icofont-simple-up"></i></a>
-            <div id="faq2" class="collapse" data-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-toggle="collapse" href="#faq3" class="collapsed">Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi? <i class="icofont-simple-up"></i></a>
-            <div id="faq3" class="collapse" data-parent=".faq-list">
-              <p>
-                Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-toggle="collapse" href="#faq4" class="collapsed">Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla? <i class="icofont-simple-up"></i></a>
-            <div id="faq4" class="collapse" data-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-toggle="collapse" href="#faq5" class="collapsed">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="icofont-simple-up"></i></a>
-            <div id="faq5" class="collapse" data-parent=".faq-list">
-              <p>
-                Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-              </p>
-            </div>
-          </li>
-
-          <li>
-            <a data-toggle="collapse" href="#faq6" class="collapsed">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="icofont-simple-up"></i></a>
-            <div id="faq6" class="collapse" data-parent=".faq-list">
-              <p>
-                Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
-              </p>
-            </div>
-          </li>
-
-        </ul>
-
-      </div>
-    </section>
+    
     <section id="contact" class="contact">
       <div class="container">
 
